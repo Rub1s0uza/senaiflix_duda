7f451710ff8480d1cddf563c4c9fde20514bf263
Rub1 <121504592+Rub1s0uza@users.noreply.github.com>
2024-06-22 21:00:46 -0300

    Delete pages directory

--- pages/home.php
+++
@@ -1,6 +0,0 @@
-<div class="jumbotron">
-    <h1 class="display-4">Bem-vindo à Página Inicial!</h1>
-    <p class="lead">Esta é a página inicial do meu site.</p>
-    <hr class="my-4">
-    <p>Utilize a navegação acima para explorar outras páginas.</p>
-</div>
